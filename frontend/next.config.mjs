a99b318eadb882da3b1e7126113acefa4889be6f
Parikshith Palegar <parikshith@Parikshiths-MacBook-Air.local>
2024-11-14 08:26:30 +0530

    added frontend to docker-compose

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,12 +3,12 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/v1/:path*', // Rewrite rule for the first microservice
-        destination: 'http://localhost:4000/v1/:path*', // Destination for service 1
+        source: '/v1/:path*',
+        destination: 'http://auth-service:4000/v1/:path*', // Using auth-service service name
       },
       {
-        source: '/notes/:path*', // Rewrite rule for the second microservice
-        destination: 'http://localhost:3001/:path*', // Destination for service 2
+        source: '/notes/:path*',
+        destination: 'http://notes-service:3001/:path*', // Using notes-service service name
       }
       // Add more routes as needed
     ];
